c552e858e01699b4d35516eb17fa1ebacb6b114a
Logan Hasson <logan.hasson@gmail.com>
2015-05-26 13:12:19 -0400

    Get current lesson when no lesson provided

---
+++ lib/learn_open/opener.rb
@@ -0,0 +1,39 @@
+module LearnOpen
+  class Opener
+    attr_reader :lesson, :editor, :client
+
+    def self.run(lesson:, editor_specified:)
+      new(lesson, editor_specified).run
+    end
+
+    def initialize(lesson, editor)
+      _login, token = Netrc.read['learn-config']
+      @client  = LearnWeb::Client.new(token: token)
+
+      @lesson = lesson
+      @editor = editor
+    end
+
+    def run
+      if !lesson
+        lesson = get_current_lesson
+        puts lesson
+        exit
+      end
+      # if !lesson, get current lesson path from api
+      # if lesson, get correct lesson path from api
+      #   clone
+      #   cd
+      #
+      # if editor
+      #   if binary exists
+      #     open with binary
+    end
+
+    private
+
+    def get_current_lesson
+      client.current_lesson.github_repo
+    end
+  end
+end
