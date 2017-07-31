f2296bed9de088d0a1f7073436bd627d39a383b6
Steven Nunez <steven.nunez@gmail.com>
2017-07-31 19:37:43 -0400

    Reference lesson name without student name, bump version

--- lib/learn_open/opener.rb
+++ lib/learn_open/opener.rb
@@ -28,7 +28,7 @@ module LearnOpen
         if self.repo_dir != ENV['LAB_NAME']
           home_dir = "/home/#{ENV['CREATED_USER']}"
           File.open("#{home_dir}/.custom_commands.log", "w+") do |f|
-            f.puts %Q{{"command": "open_lab", "lab_name": "#{self.lesson}"}}
+            f.puts %Q{{"command": "open_lab", "lab_name": "#{self.repo_dir}"}}
           end
           exit
         end
