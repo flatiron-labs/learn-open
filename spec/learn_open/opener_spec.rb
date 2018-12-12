0bbca3b7116b97c33f314025f34a08536b412d55
Steven G. Harms <steven.harms@flatironschool.com>
2018-12-12 13:29:11 -0500

    Implementation

--- spec/learn_open/opener_spec.rb
+++ spec/learn_open/opener_spec.rb
@@ -218,6 +218,45 @@ describe LearnOpen::Opener do
                                        io: io)
         expect { opener.run }.to raise_error(IOError)
       end
+      it "handles the carriage return character as valid" do
+        environment = {
+          "SHELL" => "/usr/local/bin/fish",
+          "LAB_NAME" => "later_lesson",
+          "CREATED_USER" => "bobby",
+          "IDE_CONTAINER" => "true",
+          "IDE_VERSION" => "3"
+        }
+        io = instance_double(LearnOpen::Adapters::IOAdapter)
+        expect(io)
+          .to receive(:puts)
+          .with("Looking for lesson...")
+
+        expect(io)
+          .to receive(:puts)
+          .with("WARNING: You are attempting to open a lesson that is beyond your current lesson.")
+
+        expect(io)
+          .to receive(:print)
+          .with("Are you sure you want to continue? [Y/n]: ")
+
+        expect(io)
+          .to receive(:gets)
+          .and_return("\n")
+
+        expect(io)
+          .to receive(:puts)
+          .with("Opening readme...")
+
+        create_linux_home_dir("bobby")
+        opener = LearnOpen::Opener.new("later_lesson", "atom", true, false,
+                                       learn_web_client: learn_web_client,
+                                       git_adapter: git_adapter,
+                                       environment_vars: environment,
+                                       system_adapter: system_adapter,
+                                       git_ssh_connector: git_ssh_connector,
+                                       io: io)
+        opener.run
+      end
       it "prompts if they want to skip lesson" do
         environment = {
           "SHELL" => "/usr/local/bin/fish",
